6228cb7fca51684bf54782defdd6605d760c27fb
xiang0105 <110757121+xiang0105@users.noreply.github.com>
2023-10-25 21:38:42 +0800

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int
+main ()
+{
+    // 1-2
+   int inp , acc , pass;
+   scanf("%d",&inp);
+
+   for(int i = 0 ; i < inp ; i++){
+       scanf("%d %d",&acc , &pass);
+       if(acc == 123 && pass == 456){
+           printf("9000\n");
+       }else if(acc == 456 && pass == 789){
+           printf("5000\n");
+       }else if(acc == 789 && pass == 888){
+           printf("6000\n");
+       }else if(acc == 336 && pass == 558){
+           printf("10000\n");
+       }else if(acc == 775 && pass == 666){
+           printf("12000\n");
+       }else if(acc == 566 && pass == 221){
+           printf("7000\n");
+       }else{
+           printf("error\n");
+       }
+   }
+
+
+}
 
